eb286dcb15eb06bcc3a31fb2525257895951a7bd
ThijsPR <tavandebelt@gmail.com>
2024-02-27 15:45:01 +0100

    player class

--- Player.cs
+++ Player.cs
@@ -1,8 +1,17 @@
-class RecoverHealth()
+public class player
 {
-    int maxHealt = 100
-    While (true)
-    {
+    public int CurrentHitPoints;
+    public string CurrentLocation;
+    public string CurrentWeapon;
+    public string MaximumHitPoints;
+    public string Name;
 
+    public player(int currentHitPoints, string currentLocation, string currentWeapon, string maximumHitPoints, string name)
+    {
+        this.CurrentHitPoints = currentHitPoints;
+        this.CurrentLocation = currentLocation;
+        this.CurrentWeapon = currentWeapon;
+        this.MaximumHitPoints = maximumHitPoints
+        this.Name = name
     }
 }
