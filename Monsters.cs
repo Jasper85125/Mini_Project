7aa38b3b554db69e1178fdd3471c4a794d9778ea
Amer Alhasoun <ameralhasoun@gmail.com>
2024-02-27 14:31:56 +0100

    Compass class added

--- Monsters.cs
+++ Monsters.cs
@@ -1,6 +1,6 @@
-public class Monster
-{
-    public int healthpoints = 50;
-    public int attackpoints = 5;
-    public
-}
+// public class Monster
+// {
+//     public int healthpoints = 50;
+//     public int attackpoints = 5;
+//     public
+// }
