7ef226dba1b1c5fb456f9ad8b98a0da5115f52e8
ThijsPR <tavandebelt@gmail.com>
2024-02-27 15:55:12 +0100

    added recoverHealth class and added 2 things to the player class

--- Player.cs
+++ Player.cs
@@ -1,14 +1,16 @@
 public class player
 {
     public int CurrentHitPoints;
+    public int CurrentHealthPotions
     public string CurrentLocation;
     public string CurrentWeapon;
     public string MaximumHitPoints;
     public string Name;
 
-    public player(int currentHitPoints, string currentLocation, string currentWeapon, string maximumHitPoints, string name)
+    public player(int currentHitPoints, int currentHealthPotions, string currentLocation, string currentWeapon, string maximumHitPoints, string name)
     {
         this.CurrentHitPoints = currentHitPoints;
+        this.CurrentHealthPotions = currentHealthPotions;
         this.CurrentLocation = currentLocation;
         this.CurrentWeapon = currentWeapon;
         this.MaximumHitPoints = maximumHitPoints;
