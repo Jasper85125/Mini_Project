65579cac443c3bf7d3fcba862bcb8667c3fa526a
Jasper van der Laan <Jaspervanderlaan1105@gmail.com>
2024-02-27 15:14:17 +0100

    update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,29 @@
-﻿
+﻿Monster monster = new();
+string input = Console.ReadLine();
+int input2 = Convert.ToInt32(input);
+int attack = 5;
+switch (input2) 
+{
+    case 1:
+        Monster.attack(attack);
+        Console.WriteLine($"You did {attack} damage");
+        Console.WriteLine(monster.healthpoints);
+        break;
+    case 2:
+        
+        break;
+    case 3:
+        
+        break;
+    case 4:
+        
+        break;
+    default:
+        break;
+
+}
+
+
 string =North 
 Compass compass = new("Y", "Y", "Y", "Y");
 compass.DrawCompass();
