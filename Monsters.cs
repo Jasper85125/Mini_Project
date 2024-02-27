e1fc3e76d22c110d46426783c98c54468b71436e
Jasper van der Laan <Jaspervanderlaan1105@gmail.com>
2024-02-27 14:33:25 +0100

    monster update

--- Monsters.cs
+++ Monsters.cs
@@ -2,5 +2,15 @@ public class Monster
 {
     public int healthpoints = 50;
     public int attackpoints = 5;
-    public
+    public string name_monster = "Rat";
+
+    public void attack(int dmg)
+    {
+        healthpoints -= dmg;
+    }
+
+    public void monter_attack()
+    {
+        break;
+    }
 }
