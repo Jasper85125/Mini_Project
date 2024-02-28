36a75987f05f45f1eb4c80ce188a83eec39df3ad
Stéphane Dauvillier <dauvillier04@gmail.com>
2024-02-28 13:16:39 +0100

    LocationToDirection changes

--- Location.cs
+++ Location.cs
@@ -1,3 +1,6 @@
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+
 public class Location
 {
     public int ID;
@@ -15,23 +18,109 @@ public class Location
         this.Option_2 = option_2;
     }
 
-    public void LocationToNorth()
+    public void LocationToNorth(Player player)
     {
-        
+        if (this.ID == 1)
+        {
+            player.CurrentLocation = "2";
+        }
+        else if (this.ID == 2)
+        {
+            player.CurrentLocation = "4";
+        }
+        else if (this.ID == 4)
+        {
+            player.CurrentLocation = "5";
+        }
+        else
+        {
+            Console.WriteLine("Can't go North");
+        } 
     }
 
-    public void LocationToEast()
+    public void LocationToEast(Player player)
+    {
+        if (this.ID == 7)
+        {
+            player.CurrentLocation = "6";
+        }
+        else if (this.ID == 6)
+        {
+            player.CurrentLocation = "2";
+        }
+        else if (this.ID == 2)
+        {
+            player.CurrentLocation = "3";
+        }
+        else if (this.ID == 3)
+        {
+            player.CurrentLocation = "8";
+        }
+        else if (this.ID == 8)
+        {
+            player.CurrentLocation = "9";
+        }
+        else
+        {
+            Console.WriteLine("Can't go East");
+        } 
+    }
+
+    public void LocationToSouth(Player player)
+    {
+        if (this.ID == 5)
+        {
+            player.CurrentLocation = "4";
+        }
+        else if(this.ID == 4)
+        {
+            player.CurrentLocation = "2";
+        }
+        else if (this.ID == 2)
+        {
+            player.CurrentLocation = "1";
+        }
+        else
+        {
+            Console.WriteLine("Can't go South");
+        }
+    }
+
+    public void LocationToWest(Player player)
+    {
+        if (this.ID == 9)
+        {
+            player.CurrentLocation = "8";
+        }
+        else if (this.ID == 8)
+        {
+            player.CurrentLocation = "3";
+        }
+        else if (this.ID == 3)
+        {
+            player.CurrentLocation = "2";
+        }
+        else if (this.ID == 2)
+        {
+            player.CurrentLocation = "6";
+        }
+        else if (this.ID == 6)
+        {
+            player.CurrentLocation = "7";
+        }
+        else
+        {
+            Console.WriteLine("Can't go West");
+        }  
+    }
+
+    public void QuestAvailableHere()
     {
 
     }
 
-    public void LocationToSouth()
+    public void MonsterLivingHere()
     {
 
-    }
-
-    public void LocationToWest()
-    {
-        
     }
 }
