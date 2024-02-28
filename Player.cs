7ea862d504a6dac17bea6e72d1e97416fbfb071b
ThijsPR <tavandebelt@gmail.com>
2024-02-28 11:14:23 +0100

    fixes

--- Player.cs
+++ Player.cs
@@ -1,13 +1,13 @@
-public class player
+public class Player
 {
     public int CurrentHitPoints;
-    public int CurrentHealthPotions
+    public int CurrentHealthPotions;
     public string CurrentLocation;
     public string CurrentWeapon;
     public string MaximumHitPoints;
     public string Name;
 
-    public player(int currentHitPoints, int currentHealthPotions, string currentLocation, string currentWeapon, string maximumHitPoints, string name)
+    public Player(int currentHitPoints, int currentHealthPotions, string currentLocation, string currentWeapon, string maximumHitPoints, string name)
     {
         this.CurrentHitPoints = currentHitPoints;
         this.CurrentHealthPotions = currentHealthPotions;
--- RecoverHealth.cs
+++ RecoverHealth.cs
@@ -1,6 +1,27 @@
 public class RecoverHealth
 {
-    public int CurrentHitPoints
-    public int CurrentHealthPotions
-    public int MaximumHitPoints
+    public int CurrentHitPoints;
+    public int CurrentHealthPotions;
+    public int MaximumHitPoints;
+    public int HealthPerPotion;
+
+    public RecoverHealth(int currentHitPoints, int currentHealthPotions, int maximumHitPoints, int healthPerPotion = 25)
+    {
+        this.CurrentHitPoints = currentHitPoints;
+        this.CurrentHealthPotions = currentHealthPotions;
+        this.MaximumHitPoints = maximumHitPoints;
+        this.HealthPerPotion = healthPerPotion;
+    }
+
+    public void UseHealthPotion()
+    {
+        CurrentHitPoints += HealthPerPotion;
+        CurrentHealthPotions--;
+        return CurrentHitPoints, CurrentHealthPotions;
+    }
+
+    public void RestAtCamp()
+    {
+        // Implementation for resting at camp
+    }
 }
