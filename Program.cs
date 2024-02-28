5c01fba4bbebf9a19a9541a1b19d61d0d0d8ab30
Jasper van der Laan <Jaspervanderlaan1105@gmail.com>
2024-02-28 15:43:42 +0100

    story progression

--- Program.cs
+++ Program.cs
@@ -5,19 +5,53 @@ public static class Program
 {
     public static void Main()
     {
+        //make compass
         string GoNorth = "Y";
         string GoEast = "N";
         string GoSouth = "N";
         string GoWest = "N";
         Compass compass = new Compass(north: GoNorth, east: GoEast, south: GoSouth, west: GoWest);
-        compass.DrawCompass();
 
-        Player player = new(5, 1, 1, 10, "Steve");
-        // Console.WriteLine(Location home);
-        Location home = new Location(1, "Home", "Your house. You really need to clean up the place.", null, null);
-        Console.WriteLine(home.Description);
-        Console.WriteLine(player);
-        Console.WriteLine("");
+        Console.WriteLine("Welkome to our game what is your name.");
+        string name_of_player = Console.ReadLine();
+        Player player = new(5, 1, 1, 10, name_of_player);
+
+        Console.WriteLine($"Welkome {player.Name}");
+
+        while(player.CurrentLocation == 1)
+        {
+            //make location
+            //Location home = new Location(1, "Home", "Your house. You really need to clean up the place.", null, null);
+
+            //draw compass
+            compass.DrawCompass();
+
+            //start the writing
+            Console.WriteLine("You are currently home what would you like to do.");
+            Console.WriteLine("1: look around");
+            Console.WriteLine("2: leave the house");
+            Console.WriteLine("3: go back to bed");
+            string choice1 = Console.ReadLine();
+            if(choice1 == "1")
+            {
+                Console.WriteLine(home.Description);
+            }
+            else if(choice1 == "2")
+            {
+                Console.WriteLine("you open the door and walk to Town square");
+                home.LocationToNorth;
+                Console.WriteLine(player.CurrentLocation);
+            }
+            else if(choice1 == "3")
+            {
+                Console.WriteLine("You go back to bed, but cant sleep better go do somting");
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid input");
+            }
+        }
+        
 
         GoNorth = "Y";
         GoEast = "Y";
