e468d1021b4c95650344a1de2a0178d677248679
Stéphane Dauvillier <dauvillier04@gmail.com>
2024-02-28 14:03:20 +0100

    goated work

--- Location.cs
+++ Location.cs
@@ -8,6 +8,10 @@ public class Location
     public string Description;
     public string Option_1;
     public string Option_2;
+    public Location LocationToNorth;
+    public Location LocationToEast;
+    public Location LocationToSouth;
+    public Location LocationToWest;
 
     public Location(int id, string name, string description, string option_1, string option_2)
     {
@@ -18,101 +22,101 @@ public class Location
         this.Option_2 = option_2;
     }
 
-    public void LocationToNorth(Player player)
-    {
-        if (this.ID == 1)
-        {
-            player.CurrentLocation = 2;
-        }
-        else if (this.ID == 2)
-        {
-            player.CurrentLocation = 4;
-        }
-        else if (this.ID == 4)
-        {
-            player.CurrentLocation = 5;
-        }
-        else
-        {
-            Console.WriteLine("Can't go North");
-        } 
-    }
+    // public void LocationToNorth(Player player)
+    // {
+    //     if (this.ID == 1)
+    //     {
+    //         player.CurrentLocation = 2;
+    //     }
+    //     else if (this.ID == 2)
+    //     {
+    //         player.CurrentLocation = 4;
+    //     }
+    //     else if (this.ID == 4)
+    //     {
+    //         player.CurrentLocation = 5;
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("Can't go North");
+    //     } 
+    // }
 
-    public void LocationToEast(Player player)
-    {
-        if (this.ID == 7)
-        {
-            player.CurrentLocation = 6;
-        }
-        else if (this.ID == 6)
-        {
-            player.CurrentLocation = 2;
-        }
-        else if (this.ID == 2)
-        {
-            player.CurrentLocation = 3;
-        }
-        else if (this.ID == 3)
-        {
-            player.CurrentLocation = 8;
-        }
-        else if (this.ID == 8)
-        {
-            player.CurrentLocation = 9;
-        }
-        else
-        {
-            Console.WriteLine("Can't go East");
-        } 
-    }
+    // public void LocationToEast(Player player)
+    // {
+    //     if (this.ID == 7)
+    //     {
+    //         player.CurrentLocation = 6;
+    //     }
+    //     else if (this.ID == 6)
+    //     {
+    //         player.CurrentLocation = 2;
+    //     }
+    //     else if (this.ID == 2)
+    //     {
+    //         player.CurrentLocation = 3;
+    //     }
+    //     else if (this.ID == 3)
+    //     {
+    //         player.CurrentLocation = 8;
+    //     }
+    //     else if (this.ID == 8)
+    //     {
+    //         player.CurrentLocation = 9;
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("Can't go East");
+    //     } 
+    // }
 
-    public void LocationToSouth(Player player)
-    {
-        if (this.ID == 5)
-        {
-            player.CurrentLocation = 4;
-        }
-        else if(this.ID == 4)
-        {
-            player.CurrentLocation = 2;
-        }
-        else if (this.ID == 2)
-        {
-            player.CurrentLocation = 1;
-        }
-        else
-        {
-            Console.WriteLine("Can't go South");
-        }
-    }
+    // public void LocationToSouth(Player player)
+    // {
+    //     if (this.ID == 5)
+    //     {
+    //         player.CurrentLocation = 4;
+    //     }
+    //     else if(this.ID == 4)
+    //     {
+    //         player.CurrentLocation = 2;
+    //     }
+    //     else if (this.ID == 2)
+    //     {
+    //         player.CurrentLocation = 1;
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("Can't go South");
+    //     }
+    // }
 
-    public void LocationToWest(Player player)
-    {
-        if (this.ID == 9)
-        {
-            player.CurrentLocation = 8;
-        }
-        else if (this.ID == 8)
-        {
-            player.CurrentLocation = 3;
-        }
-        else if (this.ID == 3)
-        {
-            player.CurrentLocation = 2;
-        }
-        else if (this.ID == 2)
-        {
-            player.CurrentLocation = 6;
-        }
-        else if (this.ID == 6)
-        {
-            player.CurrentLocation = 7;
-        }
-        else
-        {
-            Console.WriteLine("Can't go West");
-        }  
-    }
+    // public void LocationToWest(Player player)
+    // {
+    //     if (this.ID == 9)
+    //     {
+    //         player.CurrentLocation = 8;
+    //     }
+    //     else if (this.ID == 8)
+    //     {
+    //         player.CurrentLocation = 3;
+    //     }
+    //     else if (this.ID == 3)
+    //     {
+    //         player.CurrentLocation = 2;
+    //     }
+    //     else if (this.ID == 2)
+    //     {
+    //         player.CurrentLocation = 6;
+    //     }
+    //     else if (this.ID == 6)
+    //     {
+    //         player.CurrentLocation = 7;
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("Can't go West");
+    //     }  
+    // }
 
     public void QuestAvailableHere()
     {
--- Program.cs
+++ Program.cs
@@ -1,29 +1,34 @@
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
-string GoNorth = "Y";
-string GoEast = "N";
-string GoSouth = "N";
-string GoWest = "N";
-Compass compass = new Compass(north: GoNorth, east: GoEast, south: GoSouth, west: GoWest);
-compass.DrawCompass();
+public static class Program
+{
+    public static void Main()
+    {
+        string GoNorth = "Y";
+        string GoEast = "N";
+        string GoSouth = "N";
+        string GoWest = "N";
+        Compass compass = new Compass(north: GoNorth, east: GoEast, south: GoSouth, west: GoWest);
+        compass.DrawCompass();
 
-Player player = new(5, 1, 1, 10, "Steve");
-// Console.WriteLine(Location home);
-Location home = new Location(1, "Home", "Your house. You really need to clean up the place.", null, null);
-Console.WriteLine(home.Description);
-home.LocationToNorth(player);
-Console.WriteLine(player);
-Console.WriteLine("");
+        Player player = new(5, 1, 1, 10, "Steve");
+        // Console.WriteLine(Location home);
+        Location home = new Location(1, "Home", "Your house. You really need to clean up the place.", null, null);
+        Console.WriteLine(home.Description);
+        Console.WriteLine(player);
+        Console.WriteLine("");
 
-GoNorth = "Y";
-GoEast = "Y";
-GoSouth = "Y";
-GoWest = "Y";
-Compass compass2 = new Compass(north: GoNorth, east: GoEast, south: GoSouth, west: GoWest);
-compass2.DrawCompass();
+        GoNorth = "Y";
+        GoEast = "Y";
+        GoSouth = "Y";
+        GoWest = "Y";
+        Compass compass2 = new Compass(north: GoNorth, east: GoEast, south: GoSouth, west: GoWest);
+        compass2.DrawCompass();
 
-Location Town = new Location(2, "Town square", "You see a fountain.", null, null);
-Console.WriteLine(Town.Description);
-Town.LocationToEast(player);
-Console.WriteLine(player);
+        Location Town = new Location(2, "Town square", "You see a fountain.", null, null);
+        Console.WriteLine(Town.Description);
+        Console.WriteLine(player);
+    }
+}
 
